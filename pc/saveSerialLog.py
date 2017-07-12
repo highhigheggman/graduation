bf055850b877761c635a16bb570daf3b8b371a5e
highhigheggman <highhigheggman@yahoo.co.jp>
2017-07-12 14:59:54 +0900

    update

---
+++ pc/saveSerialLog.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+import serial
+import serial.tools.list_ports
+import time
+import subprocess
+from operator import attrgetter
+
+def __main():
+
+    # wait arduino bootup
+    time.sleep(3.0)
+
+    # serial setting
+    ser = serial.Serial(
+        baudrate = 9600,
+        bytesize = serial.EIGHTBITS,
+        parity = serial.PARITY_EVEN,
+        stopbits = serial.STOPBITS_ONE,
+        timeout = 1
+    )
+
+    # get a list of ports
+    ser_ls = [i for i in serial.tools.list_ports.comports() if i.location is not None]
+
+    # sort by port location
+    ser_ls.sort(key=attrgetter('location'))
+
+    while True:
+        for i in ser_ls:
+            ser.port = i.device
+            ser.open()
+
+            # wait data
+            while(ser.inWaiting() < 1):
+                time.sleep(0.1)
+
+            # read line
+            while True:
+                str = ser.readline().decode('utf-8').strip()
+                if num:
+                    break
+
+            print(str)
+            print(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+
+            ser.close()
+
+
+if __name__ =='__main__':
+    __main()
+
