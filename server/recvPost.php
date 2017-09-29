db03e93e086ed28f287a81629cb8d652f0ad0166
highhigheggman <highhigheggman@yahoo.co.jp>
2017-09-29 15:55:22 +0900

    update

--- server/recvPost.php
+++ server/recvPost.php
@@ -23,12 +23,11 @@ try {
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ]
     );
 
-    // use Prepares
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
+    // insert data to DB
     $stmt = $pdo->prepare('insert into acc_DB.sensorVal (deviceId, maxAcc, minAcc, sensorTime) values(:deviceId, :maxAcc, :minAcc, :sensorTime)');
     $stmt->bindValue(':deviceId', $deviceId, PDO::PARAM_INT);
     $stmt->bindValue(':maxAcc', $maxAcc, PDO::PARAM_STR);
