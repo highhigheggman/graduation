845f09d9fcb5e13f511b923ed08db0c57dee9bb7
highhigheggman <highhigheggman@yahoo.co.jp>
2017-09-29 15:18:10 +0900

    update

--- server/recvPost.php
+++ server/recvPost.php
@@ -5,8 +5,6 @@ $maxAcc = $_POST['maxAcc'];
 $minAcc = $_POST['minAcc'];
 $sensorTime = $_POST['time'];
 
-echo 'hello';
-
 // check POST data
 if(empty($deviceId) || empty($maxAcc) || empty($minAcc) || empty($sensorTime)) {
     echo 'error';
@@ -19,7 +17,7 @@ try {
     $pdo = new PDO(
         'mysql:dbname=acc_DB;host=localhost;charset=utf8mb4',
         'postManager',
-        '176520-Kz'
+        '176520-Kz',
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -29,7 +27,7 @@ try {
     // use Prepares
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-    $stmt = $pdo->prepare(insert into acc_DB.sensorVal (deviceID, maxAcc, minAcc, sensorTime) values(:deviceId, :maxAcc, :minAcc, :sensorTime));
+    $stmt = $pdo->prepare('insert into acc_DB.sensorVal (deviceID, maxAcc, minAcc, sensorTime) values(:deviceId, :maxAcc, :minAcc, :sensorTime)');
     $stmt->bindValue(':deviceId', $deviceId, PDO::PARAM_INT);
     $stmt->bindValue(':maxAcc', $maxAcc, PDO::PARAM_STR);
     $stmt->bindValue(':minAcc', $minAcc, PDO::PARAM_STR);
